e564a630ede4af5d122694ec4fb689061b5c399e
Mikael Karon <mikael@karon.se>
2017-06-16 03:26:27 +0200

    fix: UMD fixes
    Fix UMD wrapper to for static analysis

--- jquery.js
+++ jquery.js
@@ -1,12 +1,12 @@
-(function (modules, root, factory) {
+(function (root, factory) {
   if (typeof define === "function" && define.amd) {
-    define(modules, factory);
+    define([], factory);
   } else if (typeof module === "object" && module.exports) {
-    module.exports = factory.call(root);
+    module.exports = factory();
   } else {
-    root["mu-jquery-widget-utils/jquery"] = factory.call(root);
+    root["mu-jquery-widget-utils/jquery"] = factory();
   }
-})([], this, function () {
+})(this, function () {
   var map = Array.prototype.map;
   var blueprints = {};
   var jq = /^(?:children|closest|contents|find|next(?:All|Until)?|parent(?:s|sUntil)?|prev(?:Until)?|siblings)$/;
